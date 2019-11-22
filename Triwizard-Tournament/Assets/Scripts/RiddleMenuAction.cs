ee028a47e34c04146b44a533c31bba280e03ad4a
Tommy Yang <tommyyangmd@gmail.com>
2019-11-22 01:55:52 -0500

    added two basic riddles and images

---
+++ Triwizard-Tournament/Assets/Scripts/RiddleMenuAction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiddleMenuAction : MonoBehaviour
+{
+    public GameObject thisMenu;
+
+    //Correct choice, player continues and no points deducted.
+    public void Correct()
+    {
+        thisMenu.SetActive(false);
+    }
+
+    //Wrong choice, deduct point by 1, if player has had 3 deductions, then game over.
+    // ** NOT IMPLEMENTED YET **
+    public void Wrong()
+    {
+        thisMenu.SetActive(false);
+    }
+}
---
+++ Triwizard-Tournament/Assets/Scripts/RiddleTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiddleTrigger : MonoBehaviour
+{
+    public GameObject thisRiddle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisRiddle.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
