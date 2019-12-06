8c4f4a73ffcf65a06af59c0cfdefc39e2d5c8506
Cameron Bernhardt <cambernhardt@me.com>
2019-12-06 04:19:00 -0500

    Finishing touches

--- Triwizard-Tournament/Assets/Scripts/CountdownTimer.cs
+++ Triwizard-Tournament/Assets/Scripts/CountdownTimer.cs
@@ -9,7 +9,7 @@ public class CountdownTimer : MonoBehaviour
     public GameObject camera;
     private float countdownTimer = 600f; //10 minutes, 600 seconds
     private Text timerText;
-    private bool gameOver = false;
+    public bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,25 +21,31 @@ public class CountdownTimer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        countdownTimer -= Time.deltaTime;
+        if (!gameOver) {
+            countdownTimer -= Time.deltaTime;
 
-        //Format time to look like minutes : seconds
-        string minutes = ((int)countdownTimer / 60).ToString();
-        string seconds = (countdownTimer % 60).ToString("f0");
+            //Format time to look like minutes : seconds
+            string minutes = ((int)countdownTimer / 60).ToString();
+            string seconds = (countdownTimer % 60).ToString("f0");
+            if (seconds == "60") {
+                seconds = "0";
+                minutes = (((int)countdownTimer / 60) + 1).ToString();
+            }
 
-        if ((countdownTimer % 60) < 9) {
-            timerText.text = minutes + ":0" + seconds;
-        } else {
-            timerText.text = minutes + ":" + seconds;
-        }
+            if (seconds.Length == 1) {
+                timerText.text = minutes + ":0" + seconds;
+            } else {
+                timerText.text = minutes + ":" + seconds;
+            }
 
-        if (countdownTimer <= 0 && !gameOver) {
-            timerText.text = "Time!";
-            
-            endMenu.transform.position = camera.transform.position + camera.transform.forward * 0.75f;
-            endMenu.transform.rotation = camera.transform.rotation;
-            endMenu.SetActive(true);
-            gameOver = true;
+            if (countdownTimer <= 0) {
+                timerText.text = "Time!";
+                
+                endMenu.transform.position = camera.transform.position + camera.transform.forward * 0.75f;
+                endMenu.transform.rotation = camera.transform.rotation;
+                endMenu.SetActive(true);
+                gameOver = true;
+            }
         }
     }
 }
--- Triwizard-Tournament/Assets/Scripts/RiddleMenuAction.cs
+++ Triwizard-Tournament/Assets/Scripts/RiddleMenuAction.cs
@@ -7,6 +7,7 @@ public class RiddleMenuAction : MonoBehaviour
     public GameObject thisMenu;
     public GameObject endMenu;
     public GameObject camera;
+    public GameObject timer;
 
     //Correct choice, player continues
     public void Correct()
@@ -20,5 +21,6 @@ public class RiddleMenuAction : MonoBehaviour
         endMenu.SetActive(true);
         endMenu.transform.position = camera.transform.position + camera.transform.forward * 0.75f;
         endMenu.transform.rotation = camera.transform.rotation;
+        timer.GetComponent<CountdownTimer>().gameOver = true;
     }
 }
