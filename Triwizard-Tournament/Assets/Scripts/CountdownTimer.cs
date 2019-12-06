768d18f300d49f8c160be4032cebff5b22134f77
Tommy Yang <tommyyangmd@gmail.com>
2019-12-06 00:33:08 -0500

    countdown timer script

---
+++ Triwizard-Tournament/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public GameObject endMenu;
+    private float countdownTimer = 600f; //10 minutes, 600 seconds
+    private Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdownTimer -= Time.deltaTime;
+        timerText.text = countdownTimer.ToString("f2");
+        if (countdownTimer <= 0) {
+            
+        }
+    }
+}
