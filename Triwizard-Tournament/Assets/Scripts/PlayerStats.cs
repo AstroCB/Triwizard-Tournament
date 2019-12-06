f374c4e74c4eda73886b736595332ce0cb8b0fce
Shruti Ray <rayshruti@gmail.com>
2019-12-05 23:09:40 -0500

    Added player stats and end menu

---
+++ Triwizard-Tournament/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    private static bool lost = false;
+    
+    public bool HasLost(){
+        return lost;
+    }
+
+    public void Lost(){
+        lost = true;
+    }
+
+    public void Reset(){
+        lost = false;
+    }
+
+}
