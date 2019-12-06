4dc78f144d8052d71a04649833e8d8cdb12c0c97
Shruti Ray <rayshruti@gmail.com>
2019-12-05 22:20:13 -0500

    Added end canvas

---
+++ Triwizard-Tournament/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    private static int score = 0;
+    private bool lost = false;
+
+    public void UpdateScore(){
+        score+=1;
+    }
+
+    public void Lost(){
+        lost = true;
+    }
+
+    public bool HasLost(){
+        return lost;
+    }
+
+    public void Reset(){
+        score = 0;
+        lost = false;
+    }
+
+    public int GetScore(){
+        return score;
+    }
+}
---
+++ Triwizard-Tournament/Assets/Scripts/WinLoseText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinLoseText : MonoBehaviour
+{
+    public PlayerStats stats;
+    public Text txt;
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (stats.HasLost()){
+            txt.text = "You Lost!";
+        }
+        else {
+            txt.text = "You Won!";
+        }
+    }
+}
